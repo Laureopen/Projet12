9b5dddef997250417d87ca60056fa59856f1cc74
laurence <laur.grenouille@orange.fr>
2025-08-13 18:34:54 +0200

    Creation fichier fixtures

--- fixtures/fixtures.sql
+++ fixtures/fixtures.sql
@@ -16,3 +16,7 @@ SELECT 'gestion'
 WHERE NOT EXISTS (
     SELECT 1 FROM department WHERE name = 'gestion'
 );
+INSERT INTO users (name, email, password, department_id)
+SELECT 'Epic admin', 'admin@epicevents.com', '$2b$12$jKTdI8J6dJ9BL2NH4k2or.Hyn3J5hRP3NfLvkE0iE4qa8/uW12vo6', d.id
+FROM departments d WHERE d.name = 'gestion'AND NOT EXISTS (
+    SELECT 1 FROM users WHERE email = 'admin@epicevents.com');
